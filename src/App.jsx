8b5c610d7d664d6555f7bc2c2ca15f97036c6050
Aluno SENAI <Aluno@SN-392506.edu.fiesc.com.br>
2026-04-28 12:12:23 -0300

    criação do react

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const renderizarTela = () => {
+    //navegação
+  }
+
+  return (
+    <>
+      <section id="center">
+      </section>
+    </>
+  )
+}
+
+export default App
